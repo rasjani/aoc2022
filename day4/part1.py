31e3d1d02f841617285c386fedcace9f6c1c0356
Jani Mikkonen <jani.mikkonen@gmail.com>
2022-12-05 16:11:46 +0200

    day4

---
+++ day4/part1.py
@@ -0,0 +1,29 @@
+def get_range(elf):
+    start, end = elf.split("-")
+    return list(range(int(start), int(end)+1))
+
+data = []
+with open("input") as f:
+    data = f.read().splitlines()
+
+total = 0
+for i in data:
+    elf_a, elf_b = i.split(",",2)
+    elf_a = get_range(elf_a)
+    elf_b = get_range(elf_b)
+
+    lower = set(elf_a if elf_a[0] < elf_b[0] else elf_b)
+    higher = set(elf_a if elf_a[0] > elf_b[0] else elf_b)
+
+    #print(i)
+    #print(f"{lower} -- {higher}")
+
+    result = lower & higher
+    #print(f"{result}")
+
+    if result == lower or result == higher:
+        #print(f"MATCH")
+        total += 1
+
+
+print(total)
